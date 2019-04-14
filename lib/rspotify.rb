056fe4139100d527f9080c141c03a4547c13b0c4
Kash Nouroozi <knouroozi@gmail.com>
2019-04-14 10:13:09 -0700

    :)

--- lib/rspotify.rb
+++ lib/rspotify.rb
@@ -15,4 +15,15 @@ module RSpotify
   autoload :Track,              'rspotify/track'
   autoload :TrackLink,          'rspotify/track_link'
   autoload :User,               'rspotify/user'
+
+  def self.find(uris)
+    if uris.respond_to?(:first)
+      _, type, = uris.first.split(':')
+      ids = uris.map { |uri| uri.split(':').last }
+      RSpotify.const_get(type.classify).find(ids)
+    else
+      _, type, id = uris.split(':')
+      RSpotify.const_get(type.classify).find(id)
+    end
+  end
 end
